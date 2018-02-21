d3b3d64d4f299d0303ea27d21a4e4cd2d7718989
LM <LM@LRKEMADSEN964D>
2018-02-21 09:38:51 +0100

    Har rettet lidt i ColorBetUnitTest

--- RouletteGame.Legacy.Test.Unit/ColorBetUnitTest.cs
+++ RouletteGame.Legacy.Test.Unit/ColorBetUnitTest.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace RouletteGame.Legacy.Test.Unit
 {
+    [TestFixture]
     class ColorBetUnitTest
     {
+
     }
 }
