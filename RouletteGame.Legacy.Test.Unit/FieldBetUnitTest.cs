685b11eb7f1ad2dbd47c470cc6c83e59a722b302
AnneKastrup <annekastrup@hotmail.com>
2018-02-21 10:48:50 +0100

    test setup og Nunit pakker hentet

--- RouletteGame.Legacy.Test.Unit/FieldBetUnitTest.cs
+++ RouletteGame.Legacy.Test.Unit/FieldBetUnitTest.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace RouletteGame.Legacy.Test.Unit
 {
-    
+    [TestFixture]
     class FieldBetUnitTest
     {
+        private IBet fieldBet;
+        private FieldBet uut;
+
+        [SetUp]
+        public  void Setup()
+        {
+            fieldBet = Substitute.For<IBet>();
+            
+        }
+
+        public void WonAmount_kkk_kkk()
+        {
+            fieldBet.
+           uut.WonAmount();
+        
+
+        }
     }
 }
